7c04d512f6cbd18fe23fe34e36400b6e198e92be
elfsvet <stepanmatysik@gmail.com>
2022-10-02 23:51:31 -0400

    added a project to play with tailwing

--- client/src/weightHandler.js
+++
@@ -1,6 +0,0 @@
-// lets calculate weight from ounces to grams
-const weightOuncesToGrams = (weight) => (weight*28.34952).toFixed(2)
-const weightGramsToOunces = (weight) => (weight/28.34952).toFixed(2)
-
-console.log(weightOuncesToGrams(11.5))
-console.log(weightGramsToOunces(326))
