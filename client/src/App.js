4ee438963500c9686a18dfe093e35cfa9f927ead
elfsvet <stepanmatysik@gmail.com>
2022-10-03 00:34:43 -0400

    started from the start

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar.component';
 import Hero from './components/Hero.component';
-import {Routes, Route} from 'react-router-dom'
+import CarouselComponent from './components/Carousel.component';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div>
-    <Hero />
-    </div>
+    <Router>
+      <NavBar />
+      <div style={{width:'800px'}}>
+        <CarouselComponent />
+      </div>
+      <Routes>
+        <Route path='/' component={''} />
+      </Routes>
+    </Router>
   );
 }
 
